7576af26e0ccbff41ec30434cafab3475b6cd162
Wenderlog <kipop505g46@gmail.com>
2023-10-03 00:22:34 +0300

    Task 1.4

---
+++ Task 1.4/Task 1.4/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int array[19] = {0};
+    
+    
+    for (int i = 0; i <=9; i++) {
+        for (int j = 0 ; j <= 9; j++) {
+            for (int q = 0; q<=9; q++) {
+                int s = i + j + q;
+                if (s <= 18){
+                    array[s]++;
+                }
+            }
+        }
+    }
+    
+    long count = 0;
+    
+    for (int i = 0; i <= 18; i++ ){
+        count+= (long)array[i] * array[i];
+    }
+    
+    printf("Кол - во счасливых билетов: %ld\n", count);
+    return 0;
+}
