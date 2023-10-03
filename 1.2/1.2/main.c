6e5838a434ef83667e386524b863809c6e773a7a
Wenderlog <kipop505g46@gmail.com>
2023-10-03 17:15:22 +0300

    1.2

---
+++ 1.2/1.2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+int lenArray = scanf("%d", &lenArray);
+int array[lenArray];
+int k = 0;
+printf("введите значения элементов массива:\n");
+for (int i = 0; i < lenArray; i++) {
+scanf("%d", &array[i]);
+if (array[i] == 0) {
+k++;
+}
+}
+
+printf("Кол-во нулевых в массиве: %d\n", k);
+
+return 0;
+}
