7e8bfec01993921e6ca2a222c035c52efb913475
Wenderlog <kipop505g46@gmail.com>
2023-11-05 19:08:49 +0300

    9

---
+++ 9/9/list.h
@@ -0,0 +1,13 @@
+#ifndef LIST_H
+#define LIST_H
+
+struct Node {
+    char* key;
+    int count;
+    struct Node* next;
+};
+
+struct Node* create_node(const char* key);
+void destroy_list(struct Node* head);
+
+#endif
