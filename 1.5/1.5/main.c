06112dc4d463edea8d79980e6e16bbd1f3c5cf4d
Wenderlog <kipop505g46@gmail.com>
2023-11-05 17:53:57 +0300

    1.5

---
+++ 1.5/1.5/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100] = { ' ' };
+    int balance = 0;
+    scanf("%s", str);
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '(') {
+            balance++;
+        } else if (str[i] == ')') {
+            balance--;
+            if (balance < 0) {
+                printf("Баланс нарушен\n");
+                return 0;;
+            }
+        }
+    }
+    if (balance == 0) {
+        printf("Баланс соблюдается\n");
+    }
+    else
+    {
+        printf("Баланс нарушен\n");
+    }
+    return 0;
+}
