54d056d017d17405829d7b0c63f5e1990e1a2581
Wenderlog <kipop505g46@gmail.com>
2023-12-04 00:28:54 +0300

    Stack

---
+++ 5 (Stack)/my_stack.h
@@ -0,0 +1,24 @@
+#ifndef MY_STACK_H
+#define MY_STACK_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct stack_element{
+    void* value;
+    struct stack_element* next;
+}StackElement;
+
+typedef struct stack{
+    StackElement* top;
+} Stack;
+
+Stack* create_stack();
+void push(Stack* st, void* value, size_t element_size);
+void* pop(Stack* st);
+void* peek(Stack* st);
+bool is_empty(Stack* st);
+void free_stack(Stack* st);
+
+#endif /*MY_STACK_H*/
