22f9072ef1452b4780b77ae6e4f0a5c9d08d2057
Wenderlog <kipop505g46@gmail.com>
2023-12-03 23:48:47 +0300

    5.1

---
+++ Task5_1 /Tests.h
@@ -0,0 +1,16 @@
+#ifndef Tests_h
+
+#define Tests_h
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "my_stack.h"
+#include "my_stack.c"
+#include <stdio.h>
+#include <assert.h>
+
+void test1();
+void test2();
+void test3();
+void test4();
+
+#endif /* Tests_h */
