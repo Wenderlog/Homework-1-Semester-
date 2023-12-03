1f5462ff24878af1f042cd614658c6918e559c35
Wenderlog <kipop505g46@gmail.com>
2023-12-04 00:35:51 +0300

    5.1

---
+++ 5.1/task5_1.h
@@ -0,0 +1,12 @@
+#ifndef TASK5_1_H
+#define TASK5_1_H
+
+#define SUCCESS 0
+#define STACK_NOT_EMPTY 1
+#define STACK_EMPTY 2
+#define DIVIDE_BY_ZERO 3
+#define INVALID_CHARS 4
+
+int calculate(const char* s, int* result);
+
+#endif /*TASK5_1_H*/
